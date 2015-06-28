14fc8374f3bd18791dc11b336050dab00254d159
Simao Belchior <simaobelchior@gmail.com>
2015-06-28 02:07:24 +0100

    Fetch more data and display it
    Though some attributes are not being retrieved correctly

--- frontend/app/models/machine.js
+++ frontend/app/models/machine.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr('string')
+  name: DS.attr(),
+  status: DS.attr(),
+  diskUsage: DS.attr(),
+  tagList: DS.attr()
 });
